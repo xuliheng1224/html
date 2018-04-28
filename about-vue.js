68ede06c7af268172a637f9084db01e8065cc0fc
bblink <bblink@bblinks-MacBook-Pro.local>
2018-04-28 11:31:59 +0800

    about vue

---
+++ about-vue.js
@@ -0,0 +1,112 @@
+//1.关于vue的路由   在main.js中使用
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import routes from '...';
+const router = new VueRouter({
+    routes: [
+        { 
+          path: '',
+          name:'', 
+          component: '', 
+        }
+    ]
+  })
+
+  Vue.use(VueRouter)
+
+// 2.关于vue的传参（query,params)
+//     query是在url后面介个‘？’传参相当于ajax的get
+//     params就相当于ajax的post
+//     如果你使用params传参，当页面刷新的时候会获取不到传入的值
+//传
+this.$router.push({
+    name:'',
+    query:{
+        id:66
+    }
+})
+//取
+this.$route.query.id
+
+this.$router.push({
+    name:'',
+    params:{
+        id:555
+    }
+})
+this.$route.params.id
+
+// 3.关于vue的请求 
+
+import axios from 'axios'
+Vue.prototype.$axios=axios;
+
+import VueResource from 'vue-resource'
+Vue.use(VueResource);
+
+let postData = {
+    'companyCode':'tur',
+    'userName':'123456789123456789',
+    'password':'123456'
+}
+this.$http.post("",postData).then((res) =>{
+    console.log(response);
+}).catch( (error)=> {
+    console.log(error);
+});
+
+//4.关于vue的自定义过滤器filters， 自定义指令directives，可以看作对象，然后调用方法
+
+
+//监听watch，计算属性computed
+
+/* <div> {{val|add}}</div>
+    <input type='text' v-focus />
+filters:{
+    add(val){
+        return val + 3
+    }
+    some(score){
+        if (score < 20) {
+                score = '不合格';
+            } else if (score >= 20 && score <= 27) {
+                score = '合格';
+            } else if (score >= 28 && score <= 31) {
+                score = '良好';
+            } else if (score > 31) {
+                score = '优秀';
+            }
+            return score
+    }
+    
+    directives:{
+        focus(el){
+            el.classList.add('active')
+        }
+    }
+    
+    <div>{{info}}</div>
+    computed:{
+        info(){
+            return this.msg.split('').reverse().join('')
+        }
+    } 
+    
+    <div>{{number}}</div>
+    watch:{
+        number(new ,old){
+            console.log(new.old)
+        }
+    }
+} */
+
+//5.关于vue的ref
+//refs相当于是vue所有含有ref的一个对象
+
+// <h3 ref="p3" class="p3">ref的测试</h3>
+// <h4 ref="p4" class="p4">ref的测试</h4>
+
+// clickMe(){
+//     console.log(this.$refs)；
+//     this.$refs.p3.className = "p33333"；
+// }
---
+++ vue-test/src/components/HelloWorld.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>    
+   <h2>{{val|add}}</h2>
+   <input type="text" v-focus style="border:2px solid #000;">
+  <div>{{info}}</div>
+  <h3 ref="p3" class="p3">ref的测试</h3>
+  <h4 ref="p4" class="p3">ref的测试</h4>
+  <h1 @click="remove">{{number}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      val: 24,
+      msg:"i love you",
+      number:8
+    }
+
+  },
+  methods:{
+    remove(){
+      this.number = 0;
+    }
+  },
+  filters:{
+    add(val){
+      return val+5;
+    }
+  },
+  directives:{
+    focus(el){
+      el.classList.add('active')
+    }
+  },
+  computed:{
+    info(){
+      return this.msg.split('').reverse().join('')
+    }
+  },
+  watch:{
+    number(to,from){
+      if(to === 0){
+        console.log('ffdf')
+      }
+    }
+  },
+  mounted(){
+   this.$refs.p3.className = "p33333"
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>

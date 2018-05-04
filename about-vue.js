5d63bccf7cb03bb38fcda06770516c0f86099d69
xuliheng <32295648+xuliheng1224@users.noreply.github.com>
2018-05-04 12:03:09 +0800

    Update about-vue.js

--- about-vue.js
+++ about-vue.js
@@ -214,3 +214,27 @@ methods:{
 //         }
 
 //关于vue路由的懒加载（按需引入）
+// 懒加载：
+
+// 　　　　也叫延迟加载，即在需要的时候进行加载，随用随载。
+
+// 为什么需要懒加载？
+
+// 　　　　像vue这种单页面应用，如果没有应用懒加载，运用webpack打包后的文件将会异常的大，造成进入首页时，需要加载的内容过多，时间过长，会出啊先长时间的白屏，即使做了loading也是不利于用户体验，而运用懒加载则可以将页面进行划分，需要的时候加载页面，可以有效的分担首页所承担的加载压力，减少首页加载用时
+
+// 　　　　简单的说就是：进入首页不用一次加载过多资源造成用时过长！！！
+
+// r就是resolve
+const detail = r => require.ensure([], () => r(require('../components/list/detail')), 'list');
+// 路由也是正常的写法  这种是官方推荐的写的 按模块划分懒加载 
+const router = new Router({
+    routes: [
+        {
+           path: '/list/blog',
+           component: detail,
+           name: 'blog'
+        },
+        ...login,
+        ...mine
+    ]
+})

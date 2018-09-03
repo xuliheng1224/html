4950210078161cfdc8a5587b078e591caf6456de
liheng.xu <liheng.xu@bblink.cn>
2018-09-03 17:55:16 +0800

    mapvue

--- my-react/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Hello from './router/router';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Hello />, document.getElementById('root'));
-registerServiceWorker();

4fef2dd880bbffd9ed5d8bb190c62075bb545d0d
liheng.xu <liheng.xu@weimob.com>
2019-01-28 16:49:37 +0800

    函数节流和去抖

--- all.js
+++ all.js
@@ -1,156 +1,211 @@
 //下载pdf/excel
 fetch({
-        url: '/crm/user/downData',
-        method: 'get',
-        responseType: 'blob',
-        params: {
-          type: 1
-        },
-        success: function (res) {
-          if (res.status === 200) {
-            // let blob = new Blob([res.data], {type: 'application/pdf;charset=utf-8'}) pdf
-            let blob = new Blob([res.data], {type: 'application/vnd.ms-excel;charset=utf-8'}) // excel
-            let objectUrl = URL.createObjectURL(blob)
-            window.location.href = objectUrl
-          } else {
-            this.$message.error(res.data.msg)
-          }
-        },
-        fail: function (err) {
-          console.log(err)
-        }
-      }, this)
+	url: '/crm/user/downData',
+	method: 'get',
+	responseType: 'blob',
+	params: {
+		type: 1
+	},
+	success: function (res) {
+		if (res.status === 200) {
+			// let blob = new Blob([res.data], {type: 'application/pdf;charset=utf-8'}) pdf
+			let blob = new Blob([res.data], { type: 'application/vnd.ms-excel;charset=utf-8' }) // excel
+			let objectUrl = URL.createObjectURL(blob)
+			window.location.href = objectUrl
+		} else {
+			this.$message.error(res.data.msg)
+		}
+	},
+	fail: function (err) {
+		console.log(err)
+	}
+}, this)
+
+// 获取url参数
+function getQueryString(name) {
+	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+	var r = window.location.search.substr(1).match(reg);
+	if (r != null) return unescape(decodeURI(r[2])); return null
+}
+
+/**函数的去抖动**/
+function debounce(method, delay) {
+	var timer = null;
+	return function () {
+		var context = this, args = arguments;
+		clearTimeout(timer);
+		timer = setTimeout(function () {
+			method();
+			method.apply(context, args);
+		}, delay);
+	}
+}
+var n = 1;
+function resizehandler() {
+	console.log(++n);
+}
+window.onscroll = debounce(resizehandler, 500);
+
+/**函数节流**/
+function throttle(method, duration) {
+	var begin = new Date();
+	return function () {
+		var context = this, args = arguments, current = new Date();
+		if (current - begin >= duration) {
+			method.apply(context, args);
+			begin = current;
+		}
+	}
+}
+
+function print() {
+	console.log('hello world');
+}
+
+window.onscroll = function () {
+	debounce(print);
+};
+
+// 判断对象是否是空对象
+function emptyObj() {
+	// 怎么判断一个对象是否为空
+	var obj = {}
+	// 1
+	function judgeOb(obj) {
+		for (var a in obj) {
+			return alert('非空对象')
+		}
+		return alert('空对象')
+	}
+	//2
+	if (JSON.stringify(obj) == '{}') {
+		console.log('空对象');
+	}
+	//3
+	if (Object.keys(obj).length == 0) {
+		console.log('空对象');
+	} else {
+		console.log('非空对象');
+	}
+}
+
+// try,catch
+function add() {
+	try {
+		//程序异常，就会执行catch事件
+		window.a.b != alert(1)
+	}
+	catch{
+		alert(2)
+	}
+}
 //时间格式化：
-dateFormat: function (dateTime) {
-    Date.prototype.Format = function (fmt) {
-      var o = {
-        'M+' : this.getMonth()+1, //月份
-        'd+' : this.getDate(), //日
-        'h+' : this.getHours()%12 == 0 ? 12 : this.getHours()%12, //小时
-        'H+' : this.getHours(), //小时
-        'm+' : this.getMinutes(), //分
-        's+' : this.getSeconds(), //秒
-        'q+' : Math.floor((this.getMonth()+3)/3), //季度
-        'S' : this.getMilliseconds() //毫秒
-      }
-      var week = {
-        '0' : '/u65e5',
-        '1' : '/u4e00',
-        '2' : '/u4e8c',
-        '3' : '/u4e09',
-        '4' : '/u56db',
-        '5' : '/u4e94',
-        '6' : '/u516d'
-      }
-      if(/(y+)/.test(fmt)){
-        fmt=fmt.replace(RegExp.$1, (this.getFullYear()+'').substr(4 - RegExp.$1.length))
-      }
-      if(/(E+)/.test(fmt)){
-        fmt=fmt.replace(RegExp.$1, ((RegExp.$1.length>1) ? (RegExp.$1.length>2 ? '/u661f/u671f' : '/u5468') : '')+week[this.getDay()+''])
-      }
-      for(var k in o){
-        if(new RegExp('('+ k +')').test(fmt)){
-          fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (('00'+ o[k]).substr((''+ o[k]).length)))
-        }
-      }
-      return fmt
-    }
-    return new Date(dateTime).Format('yyyy-MM-dd hh:mm:ss')
- }
+function dateFormat(dateTime) {
+	Date.prototype.Format = function (fmt) {
+		var o = {
+			'M+': this.getMonth() + 1, //月份
+			'd+': this.getDate(), //日
+			'h+': this.getHours() % 12 == 0 ? 12 : this.getHours() % 12, //小时
+			'H+': this.getHours(), //小时
+			'm+': this.getMinutes(), //分
+			's+': this.getSeconds(), //秒
+			'q+': Math.floor((this.getMonth() + 3) / 3), //季度
+			'S': this.getMilliseconds() //毫秒
+		}
+		var week = {
+			'0': '/u65e5',
+			'1': '/u4e00',
+			'2': '/u4e8c',
+			'3': '/u4e09',
+			'4': '/u56db',
+			'5': '/u4e94',
+			'6': '/u516d'
+		}
+		if (/(y+)/.test(fmt)) {
+			fmt = fmt.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length))
+		}
+		if (/(E+)/.test(fmt)) {
+			fmt = fmt.replace(RegExp.$1, ((RegExp.$1.length > 1) ? (RegExp.$1.length > 2 ? '/u661f/u671f' : '/u5468') : '') + week[this.getDay() + ''])
+		}
+		for (var k in o) {
+			if (new RegExp('(' + k + ')').test(fmt)) {
+				fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)))
+			}
+		}
+		return fmt
+	}
+	return new Date(dateTime).Format('yyyy-MM-dd hh:mm:ss')
+}
 //获取最大值
-function getMax(){
+function getMax() {
 	var max = arguments[0];
-	for(var i=0;i<arguments.length;i++){
-		if(max<=arguments[i]){
-			max=arguments[i]
+	for (var i = 0; i < arguments.length; i++) {
+		if (max <= arguments[i]) {
+			max = arguments[i]
 		}
 	}
 	return max;
 }
 
 //获取最小值
-function getMin(){
-	var min =arguments[0];
-	for(var i=0;i<arguments.length;i++){
-		if(min>=arguments[i]){
-			min=arguments[i]
+function getMin() {
+	var min = arguments[0];
+	for (var i = 0; i < arguments.length; i++) {
+		if (min >= arguments[i]) {
+			min = arguments[i]
 		}
 	}
 	return min;
 }
 
 //随机数 n小于m
-function random1Num(n,m){
-	return parseInt(n+Math.random()*(m-n+1));
+function random1Num(n, m) {
+	return parseInt(n + Math.random() * (m - n + 1));
 }
 
 //打乱随机排序
-
-function random1Number(arr){
-	arr.sort(function(){
-		return 0.5-Math.random();
+function random1Number(arr) {
+	arr.sort(function () {
+		return 0.5 - Math.random();
 	})
-
 	return arr;
 }
 
-//返回最大值   传入的参数是数组
-function getMax(arr){
-	var max = arr[0];
-	for(var i=0;i<arr.length;i++){
-		if(max<arr[i]){
-			max=arr[i]
-		}
-	}
-	return max;
-}
-
-//返回最小值   传入的参数是数组
-function getMin(arr){
-	var min = arr[0];
-	for(var i=0;i<arr.length;i++){
-		if(min>arr[i]){
-			min=arr[i]
-		}
-	}
-	return min;
-}
-
 //返回最大值的下标  传入的是数组
-function getMaxIndex(arr){
+function getMaxIndex(arr) {
 	var max = arr[0];
 	var index;
-	for(var i=0;i<arr.length;i++){
-		if(max<arr[i]){
-			max=arr[i]
-			index=i;
+	for (var i = 0; i < arr.length; i++) {
+		if (max < arr[i]) {
+			max = arr[i]
+			index = i;
 		}
 	}
 	return index;
 }
 
 //返回最小下标  传入的是数组
-function getMinIndex(arr){
-	var min = arr[arr.length-1];
-	var index=arr.length-1;
-	for(var i=0;i<arr.length;i++){
-		if(min>arr[i]){
-			min=arr[i];
-			index=i;
+function getMinIndex(arr) {
+	var min = arr[arr.length - 1];
+	var index = arr.length - 1;
+	for (var i = 0; i < arr.length; i++) {
+		if (min > arr[i]) {
+			min = arr[i];
+			index = i;
 		}
 	}
 	return index;
 }
 
 //冒泡排序  传入的是数组
-function bubbling(arr){
+function bubbling(arr) {
 	var temp;
-	for(var i=0;i<arr.length-1;i++){
-		for(var j=0;j<arr.length-1-i;j++){
-			if(arr[j]>arr[j+1]){
+	for (var i = 0; i < arr.length - 1; i++) {
+		for (var j = 0; j < arr.length - 1 - i; j++) {
+			if (arr[j] > arr[j + 1]) {
 				temp = arr[j];
-				arr[j]=arr[j+1];
-				arr[j+1]=temp;
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
 
 			}
 		}
@@ -159,11 +214,11 @@ function bubbling(arr){
 }
 
 //选择排序 传入的是数组
-function selectSort(arr){
+function selectSort(arr) {
 	var temp;
-	for(var i=0;i<arr.length-1;i++){
-		for(var j=i+1;j<arr.length;j++){
-			if(arr[i]>arr[j]){
+	for (var i = 0; i < arr.length - 1; i++) {
+		for (var j = i + 1; j < arr.length; j++) {
+			if (arr[i] > arr[j]) {
 				temp = arr[i];
 				arr[i] = arr[j];
 				arr[j] = temp;
@@ -174,9 +229,9 @@ function selectSort(arr){
 }
 //数组判断是否有某个值
 //传入的值是一个数组  n是一个字符
-function has(arr,n){
-	for(var i in arr){
-		if(arr[i]===n){
+function has(arr, n) {
+	for (var i in arr) {
+		if (arr[i] === n) {
 			return true;
 		}
 	}
@@ -184,10 +239,10 @@ function has(arr,n){
 }
 
 //数组去重
-function norepeat(arr){
+function norepeat(arr) {
 	var newArr = [];
-	for(var i in arr){
-		if(!has(newArr,arr[i])){
+	for (var i in arr) {
+		if (!has(newArr, arr[i])) {
 			newArr.push(arr[i])
 		}
 	}
@@ -195,213 +250,129 @@ function norepeat(arr){
 }
 
 //数组插入 按照原来顺序排序
-function sort1Arr(arr,n){			
-	for(var i=0;i<arr.length;i++){
-		if(arr[arr.length-1]<n){
-		arr.push(n);
-		}else if(arr[i]>n){
-		arr.splice(i,0,n);
-		break;
+function sort1Arr(arr, n) {
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[arr.length - 1] < n) {
+			arr.push(n);
+		} else if (arr[i] > n) {
+			arr.splice(i, 0, n);
+			break;
 		}
-		}
-	return arr	
+	}
+	return arr
 }
 //验证码  6位数的验证码
-function authCode(){
+function authCode() {
 	var str = '';
-	while(str.length<6){
-		var num = randomNum(48,122)
-	
-		if((num>57&&num<65)||(num>90&&num<97)){
-			num = randomNum(48,122)
-		}else{
-			str+=''+String.fromCharCode(num)
+	while (str.length < 6) {
+		var num = randomNum(48, 122)
+		if ((num > 57 && num < 65) || (num > 90 && num < 97)) {
+			num = randomNum(48, 122)
+		} else {
+			str += '' + String.fromCharCode(num)
 		}
-		
 	}
 	return str
 }
 
 //随机颜色
-function randomColor(){
-	var R = random1Num(0,255)
-	var G = random1Num(0,255)
-	var B = random1Num(0,255)
-	return "#"+string2Num(R,G,B) 
+function randomColor() {
+	var R = random1Num(0, 255)
+	var G = random1Num(0, 255)
+	var B = random1Num(0, 255)
+	return "#" + string2Num(R, G, B)
 }
 //转换为16进制不足补0
-function string2Num(r,g,b){
-	r = r.toString(16).length<2?"0"+r.toString(16):r.toString(16);
-	g = g.toString(16).length<2?"0"+g.toString(16):g.toString(16);
-	b = b.toString(16).length<2?"0"+b.toString(16):b.toString(16);
-	return r+g+b;
+function string2Num(r, g, b) {
+	r = r.toString(16).length < 2 ? "0" + r.toString(16) : r.toString(16);
+	g = g.toString(16).length < 2 ? "0" + g.toString(16) : g.toString(16);
+	b = b.toString(16).length < 2 ? "0" + b.toString(16) : b.toString(16);
+	return r + g + b;
 }
 
 //将时间对象转换成字符串
-function date2String(d,sign){
-	if(sign==undefined){
-		sign="/"
+function date2String(d, sign) {
+	if (sign == undefined) {
+		sign = "/"
 	}
+	return d.getFullYear() + sign + addzero((d.getMonth() + 1)) + sign + addzero(d.getDate()) + " " + addzero(d.getHours()) + ":" + addzero(d.getMinutes()) + ":" + addzero(d.getSeconds());
+}
 
-	return d.getFullYear()+sign+addzero((d.getMonth()+1))+sign+addzero(d.getDate())+" "+addzero(d.getHours())+":"+addzero(d.getMinutes())+":"+addzero(d.getSeconds());
-}
-//当数字不足两位的时候补0  应用于时间对象
-function addzero(num){
-	var str = ""+num;
-	return str.length<2?"0"+str:str;
-}
-//获取id元素
-function $(id){
-	var id = id.slice(1,id.length)
-   return document.getElementById(id)
-}
-//获取非行间样式
-//ele:对象
-//attr:属性
-function getStyle(ele,attr){
-	if(ele.currentStyle){
-		return ele.currentStyle[attr];
-	}else{
-		return getComputedStyle(ele,false)[attr]
-	}
-}
-//隐藏
-function hide(ele){
-	ele.style.display="none";
-}
-//显示
-function show(ele){
-	ele.style.display="block";
-}
-//获取设置自定义属性
-function attr(ele,type,val){
-	if(arguments.length<2){return};
-
-	if(arguments.length==2){
-		return ele.getAttribute(type);
-	}
-
-	ele.setAttribute(type,val);
-}
-//获取到前一个兄弟节点(元素)
-function prevChild(ele) {
-	 return ele.previousElementSibling;
-}
 //获取当前元素距离页面的距离
-function offset(ele){
+function offset(ele) {
 	var obj = {};
 	obj.l = ele.offsetLeft;
 	obj.t = ele.offsetTop;
 
-	while(ele.offsetParent){
+	while (ele.offsetParent) {
 		var ele = ele.offsetParent;
-		obj.l+=ele.offsetLeft;
-		obj.t+=ele.offsetTop;
+		obj.l += ele.offsetLeft;
+		obj.t += ele.offsetTop;
 	}
 	return obj;
 }
 
 //阻止浏览器默认行为
-function prevent(e){
-	e.preventDefault?e.preventDefault():e.returnValue = false;
+function prevent(e) {
+	e.preventDefault ? e.preventDefault() : e.returnValue = false;
 }
 
 //事件监听
-//第一次参数:对象
+//第一次参数: 对象
 //第二个参数：事件类型
 //第三个参数：回调函数
 //第四个参数：是否捕获
-function attach(ele,type,fn,bool){
-	if(!bool){
-		bool=false;
+function attach(ele, type, fn, bool) {
+	if (!bool) {
+		bool = false;
 	}
-	if(ele.addEventListener){
-		ele.addEventListener(type,fn,bool)
-	}else{
-		ele.attachEvent("on"+type,fn)
+	if (ele.addEventListener) {
+		ele.addEventListener(type, fn, bool)
+	} else {
+		ele.attachEvent("on" + type, fn)
 	}
 }
 
 //阻止事件冒泡
-function cancel(e){
-	e.stopPropagetion?e.stopPropagetion():e.cancelBubble = true;
-}
-
-//设置cookie
-function setCookie(_name,_val,expires){
-	var d = new Date();
-	d.setDate(d.getDate()+expires);
-	document.cookie = _name+"="+_val+";expires="+d.toGMTString();
-}
-
-//获取cookie
-function getCookie(_name){
-	var cookie = document.cookie;
-	var arr = cookie.split("; ")
-	for(var i=0;i<arr.length;i++){
-		var newArr = arr[i].split("=");
-		if(newArr[0]==_name){
-			return newArr[1];
-		}
-	}
-}
-
-//删除cookie
-function removeCookie(_name,_val){
-	setCookie(_name,_val,-1)
-}
-//获取className 无兼容性问题  
-//父级元素
-//class类名
-function getClassName(parent,aClass){
-	//获取到父元素底下所有的子元素
-	var aEle = parent.getElementsByTagName('*');
-	var arr = [];
-	//匹配传进来的这个字符是不是一个边界符
-	var reg = new RegExp('\\b'+aClass+'\\b');
-	for(var i=0;i<aEle.length;i++){
-		if(reg.test(aEle[i].className)){
-			arr.push(aEle[i])
-		}
-	}
-	return arr;
+function cancel(e) {
+	e.stopPropagetion ? e.stopPropagetion() : e.cancelBubble = true;
 }
 
 //完美运动框架
-function move(obj,json,fn){
+function move(obj, json, fn) {
 	clearInterval(obj.timer);
-	obj.timer = setInterval(function(){
+	obj.timer = setInterval(function () {
 		var bStop = true;
-		for(var attr in json){
+		for (var attr in json) {
 			var iCur = 0;
-			if(attr=='opacity'){
-				iCur = parseInt(parseFloat(getStyle(obj,attr))*100)
-			}else{
-				iCur = parseInt(getStyle(obj,attr))
+			if (attr == 'opacity') {
+				iCur = parseInt(parseFloat(getStyle(obj, attr)) * 100)
+			} else {
+				iCur = parseInt(getStyle(obj, attr))
 			}
 
-			if(iCur!=json[attr]){
-				bStop=false;
+			if (iCur != json[attr]) {
+				bStop = false;
 			}
 
 
-			var speed = (json[attr]-iCur)/8;
-			speed = speed>0?Math.ceil(speed):Math.floor(speed);
+			var speed = (json[attr] - iCur) / 8;
+			speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
 
-			
-			if(attr=='opacity'){
-				obj.style.opacity = (iCur+speed)/100;
-				obj.style.filter = 'alpha(opacity:'+(iCur+speed)+')'
-			}else{
-				obj.style[attr]=iCur+speed+'px';
+
+			if (attr == 'opacity') {
+				obj.style.opacity = (iCur + speed) / 100;
+				obj.style.filter = 'alpha(opacity:' + (iCur + speed) + ')'
+			} else {
+				obj.style[attr] = iCur + speed + 'px';
 			}
 		}
 
-		if(bStop){
+		if (bStop) {
 			clearInterval(obj.timer);
-			if(fn){
+			if (fn) {
 				fn();
 			}
 		}
-	},30)
+	}, 30)
 }
--- fetch.js
+++ fetch.js
@@ -1,32 +1,24 @@
 // 使用promise 
 // post方式
-export function  post(url,data){
-       
- 	return new Promise((resolve,reject)=>{
-            
-	fetch(url,{
-    
-                method:'POST',
- 
-               headers:{
- 
-                  'Content-type':'application/json'
-  
-              },
-               
-	 body:JSON.stringify(data)
-            })
- .then( res => res.json())
- .then( data => resolve(data))
-.catch(err => reject(err))
+export function post(url, data) {
+
+  return new Promise((resolve, reject) => {
+
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(data => resolve(data))
+      .catch(err => reject(err))
   })
- }
+}
 
 // 调用
-http.post('http://jsonplaceholder.typicode.com/users',data)
-        .then(data => console.log(data))
-        .catch(err => console.log(err))
-
+http.post('http://jsonplaceholder.typicode.com/users', data).then(data => console.log(data)).catch(err => console.log(err))
 
 export function _get(url, params) {
   if (params) {
@@ -34,49 +26,50 @@ export function _get(url, params) {
     //拼接参数
     Object.keys(params).forEach(key => paramsArray.push(key + '=' + params[key]))
     if (url.search(/\?/) === -1) {
-        url += '?' + paramsArray.join('&')
+      url += '?' + paramsArray.join('&')
     } else {
-        url += '&' + paramsArray.join('&')
+      url += '&' + paramsArray.join('&')
     }
   }
   var result = fetch(_url + url, {
-      method: 'GET',
-      headers: {
-          token: token,
-          'Content-Type': 'application/json',
-      }})
-  return result;  
+    method: 'GET',
+    headers: {
+      token: token,
+      'Content-Type': 'application/json',
+    }
+  })
+  return result;
 }
 
 export function _post(url, formdata) {
   var result = fetch(_url + url, {
-    method:"POST",
-    headers:{
+    method: "POST",
+    headers: {
       'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
       token: token
     },
-    body:formdata
+    body: formdata
   })
-  return result; 
+  return result;
 }
 
 export function _put(url, formdata) {
   var result = fetch(_url + url, {
-    method:"PUT",
-    headers:{
+    method: "PUT",
+    headers: {
       // 'Content-Type':'application/x-www-form-urlencoded',
       token: token
     },
-    body:formdata
+    body: formdata
   })
-  return result; 
+  return result;
 }
 
-export function _delete(url){
+export function _delete(url) {
   // console.log(id)
   var result = fetch(_url + url, {
-    method:"DELETE",
-    headers:{
+    method: "DELETE",
+    headers: {
       // 'Content-Type':'application/json',
       token: token
     }
@@ -86,41 +79,40 @@ export function _delete(url){
 
 // 表单提交：post 
 Object.keys(values).forEach((key) => {
-          if (!values[key]) {
-            delete values[key]
-          }
-        })
-        console.log(values)
-        let formdata = Object.keys(values).map(key=>encodeURIComponent(key)+'='+encodeURIComponent(values[key])) .join('&');
-_post('/canal-instance', formdata).then((res)=>{
-          return res.json()
-        }).then((res)=>{
-            // 请求到的数据
-            console.log(res)
-            if (res.success) {
-              _this.props.history.push('/server/canal')
-            } else {
-              message.error(res.msg)
-            }
-        })
+  if (!values[key]) {
+    delete values[key]
+  }
+})
+let formdata = Object.keys(values).map(key => encodeURIComponent(key) + '=' + encodeURIComponent(values[key])).join('&');
+_post('/canal-instance', formdata).then((res) => {
+  return res.json()
+}).then((res) => {
+  // 请求到的数据
+  console.log(res)
+  if (res.success) {
+    _this.props.history.push('/server/canal')
+  } else {
+    message.error(res.msg)
+  }
+})
 
 // put ：
 Object.keys(values).forEach((key) => {
-          if (!values[key]) {
-            delete values[key]
-          }
-        })
-        let formdata = new FormData();
-        formdata.append('id',_this.props.match.params.id)
-        Object.keys(values).forEach(key => formdata.append(key,values[key]))
-        _put('/canal-instance',formdata).then((res)=>{
-          return res.json()
-        }).then((res)=>{
-            // 请求到的数据
-            console.log(res)
-            if (res.success) {
-              _this.props.history.push('/server/canal')
-            } else {
-              message.error(res.msg)
-            }
-        })
+  if (!values[key]) {
+    delete values[key]
+  }
+})
+let formdata = new FormData();
+formdata.append('id', _this.props.match.params.id)
+Object.keys(values).forEach(key => formdata.append(key, values[key]))
+_put('/canal-instance', formdata).then((res) => {
+  return res.json()
+}).then((res) => {
+  // 请求到的数据
+  console.log(res)
+  if (res.success) {
+    _this.props.history.push('/server/canal')
+  } else {
+    message.error(res.msg)
+  }
+})

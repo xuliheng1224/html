4787c4ba95ffc1452c7ffafd725752815a39dd43
liheng.xu <xuliheng1224@163.com>
2019-04-15 19:16:18 +0800

    fd

--- about-react.js
+++ about-react.js
@@ -1,17 +1,20 @@
+// redux: view直接触发dispatch；将action发送到reducer中后，根节点上会更新state，进而改变全局view。
+
+
 /*1. 使用官方脚手架搭建
   npm install -g create-react-app 
   create-react-app my-react
 
 2. 路由*/
-import React, {Component} from 'react'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import App from '../App'
 import First from '../pages/first'
 import Second from '../pages/second'
 import Notfound from '../pages/notfound'
 import Third from '../pages/third';
 export default class Hello extends Component {
-  render () {
+  render() {
     return (
       <Router>
         <div>
@@ -60,24 +63,24 @@ export default class Hello extends Component {
   this.props.history.push(path); // 页面刷新会报错  */
 // 4. 初始化
 class App extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
-      name : '许立恒',
-      age : 24
+      name: '许立恒',
+      age: 24
     }
   }
-}  
-  /*es6 创建组建中删除了这个函数 要想使用 可用
-  var App = React.createClass({
-    getDefaultProps(){
-      return {
-        name : '许立恒',
-        age : 24,
-        num : 0
-      }
+}
+/*es6 创建组建中删除了这个函数 要想使用 可用
+var App = React.createClass({
+  getDefaultProps(){
+    return {
+      name : '许立恒',
+      age : 24,
+      num : 0
     }
-  })*/ 
+  }
+})*/
 // 5. this的绑定
 // onClick={this.setNum.bind(this)}  
 
@@ -85,8 +88,8 @@ class App extends Component {
 this.setState({
   speed: 40
 }, () => {
-    this.roll()
-    this.setState({
+  this.roll()
+  this.setState({
     click: true
   })
 })
--- my-react/src/pages/add.js
+++ my-react/src/pages/add.js
@@ -1,28 +1,28 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 export default class Add extends Component {
-	state = {
+  state = {
     isTrue: false,
   }
-  render () {
-  	var _this = this
-    function type(){
-    	_this.setState({
-    		isTrue: !_this.state.isTrue
-    	})
-    }~
+  render() {
+    var _this = this
+    function type() {
+      _this.setState({
+        isTrue: !_this.state.isTrue
+      })
+    }
     let isTrue;
     if (_this.state.isTrue) {
-    	isTrue = 'block'
+      isTrue = 'block'
     } else {
-    	isTrue = 'none'
+      isTrue = 'none'
     }
     return (
-      <div>    		
-         	<p onClick={type}>新增的页面({_this.props.haha})</p>
-         	<div className='box' style={{display: isTrue}}>
-         		<img src='../assets/login.png' />
-         	</div>
+      <div>
+        <p onClick={type}>新增的页面({_this.props.haha})</p>
+        <div className='box' style={{ display: isTrue }}>
+          <img src='../assets/login.png' alt="dfdfd" />
+        </div>
       </div>
     )
   }
